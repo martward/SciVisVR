979adc20ca94186ed5be30c29854bd63dcac4614
Sebastien <sebastien@dutchnaoteam.nl>
2016-09-20 16:31:42 +0200

    Added streamtracer and tubes to wervel data vis.

--- Visualizations/Wervel/Wervel.cxx
+++ Visualizations/Wervel/Wervel.cxx
@@ -1,32 +1,16 @@
 #include <vtkVersion.h>
-#include <vtkPointSource.h>
-#include <vtkKdTreePointLocator.h>
-#include <vtkSmartPointer.h>
-#include <vtkPolyData.h>
-#include <vtkSliderWidget.h>
-#include <vtkPolyDataMapper.h>
-#include <vtkActor2D.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkPolyData.h>
-#include <vtkSmartPointer.h>
-#include <vtkCommand.h>
-#include <vtkWidgetEvent.h>
-#include <vtkCallbackCommand.h>
-#include <vtkWidgetEventTranslator.h>
-#include <vtkInteractorStyleTrackballCamera.h>
-#include <vtkSliderWidget.h>
-#include <vtkSliderRepresentation2D.h>
-#include <vtkProperty.h>
-#include <vtkMath.h>
-#include <vtkConeSource.h>
-#include <vtkImageReader.h>
-#include <vtkBMPReader.h>
-#include <vtkImageMapper.h>
-#include <vtkInteractorStyleImage.h>
 #include <vtkStructuredPointsReader.h>
 #include <vtkImageDataGeometryFilter.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkStreamTracer.h>
+#include <vtkStructuredGridReader.h>
+#include <vtkPointSource.h>
+#include <vtkRungeKutta4.h>
+#include <vtkTubeFilter.h>
+
 
 std::string fileName;
 
@@ -45,15 +29,38 @@ int main (int argc, char *argv[])
     pointsReader->SetFileName(fileName.c_str());
     pointsReader->Update();
 
+    vtkSmartPointer<vtkPointSource> pointSource = vtkSmartPointer<vtkPointSource>::New();
+    pointSource->SetRadius(10.0);
+    pointSource->SetNumberOfPoints(1000);
+
+    vtkSmartPointer<vtkRungeKutta4> integ = vtkSmartPointer<vtkRungeKutta4>::New();
+
+
+    vtkSmartPointer<vtkStreamTracer> streamTracer = vtkSmartPointer<vtkStreamTracer>::New();
+    streamTracer->SetInputConnection(pointsReader->GetOutputPort());
+    streamTracer->SetSourceConnection(pointSource->GetOutputPort());
+    streamTracer->SetMaximumPropagation(100);
+    streamTracer->SetInitialIntegrationStep(0.1);
+    streamTracer->SetIntegrationDirectionToBoth();
+    streamTracer->SetIntegrator(integ);
+
+    vtkSmartPointer<vtkTubeFilter> tubeFilter = vtkSmartPointer<vtkTubeFilter>::New();
+    tubeFilter->SetInputConnection(streamTracer->GetOutputPort());
+    tubeFilter->SetRadius(0.1);
+    tubeFilter->SetNumberOfSides(50);
+    tubeFilter->Update();
+
+    /*
     vtkSmartPointer<vtkImageDataGeometryFilter> geometryFilter =
             vtkSmartPointer<vtkImageDataGeometryFilter>::New();
     geometryFilter->SetInputConnection(pointsReader->GetOutputPort());
     geometryFilter->Update();
+     */
 
     // Visualize
     vtkSmartPointer<vtkPolyDataMapper> mapper =
             vtkSmartPointer<vtkPolyDataMapper>::New();
-    mapper->SetInputConnection(geometryFilter->GetOutputPort());
+    mapper->SetInputConnection(tubeFilter->GetOutputPort());
 
     vtkSmartPointer<vtkActor> actor =
             vtkSmartPointer<vtkActor>::New();
@@ -69,7 +76,6 @@ int main (int argc, char *argv[])
     renderWindowInteractor->SetRenderWindow(renderWindow);
 
     renderer->AddActor(actor);
-    renderer->SetBackground(.3, .6, .3); // Background color green
 
     renderWindow->Render();
     renderWindowInteractor->Start();
